9044c7451632cfe8a121328f105d2587815898b4
cbeatty42 <cbeattyxy@gmail.com>
2024-04-21 14:57:55 -0400

    Re-organized student info form to be more intuitive.

--- app/src/main/java/com/lifeline/fragments/StudentForm.kt
+++ app/src/main/java/com/lifeline/fragments/StudentForm.kt
@@ -5,11 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import com.lifeline.R
 import com.lifeline.databinding.FragmentStudentFormBinding
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.lifeline.ApiUtils
+import com.lifeline.SessionManager
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.json.JSONObject
+import java.math.BigInteger
+
 class StudentForm : Fragment() {
 
     private lateinit var binding: FragmentStudentFormBinding
@@ -28,9 +35,35 @@ class StudentForm : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val searchButton: Button = requireView().findViewById(R.id.button_search)
-        searchButton.setOnClickListener {
-            navigateToFragment(ScannerFragment.newInstance())
+
+
+        val token = SessionManager.fetchAuthToken(requireContext())
+        val decodedToken = token?.let {  JSONObject(SessionManager.decodeToken(it)) }
+        val id = decodedToken?.getLong("id")
+
+        if(id != null) {
+            viewLifecycleOwner.lifecycleScope.launch(Dispatchers.Main) {
+                val studentData = ApiUtils.searchStudentId(BigInteger.valueOf(id), requireContext())
+                val firstName = studentData?.full_name?.first_name?:""
+                val lastName = studentData?.full_name?.last_name?:""
+
+                binding.textViewUsername.text = "$firstName $lastName"
+
+                binding.editTextFirstName.setText(firstName)
+                binding.editTextLastName.setText(lastName)
+
+                binding.editTextEmergencyFirstName.setText(studentData?.emergency_contact?.e_first_name)
+                binding.editTextEmergencyLastName.setText(studentData?.emergency_contact?.e_last_name)
+                binding.editTextEmergencyRelation.setText(studentData?.emergency_contact?.relation)
+                binding.editTextEmergencyPhone.setText(studentData?.emergency_contact?.phone_number)
+
+                binding.studentId.setText(studentData?.medical_info?.student_id.toString())
+                binding.editTextDob.setText(studentData?.medical_info?.dob)
+            }
+        }
+        else{
+            // Pop the back stack if we don't have a defined id since we shouldn't be here if it's not
+            parentFragmentManager.popBackStack()
         }
     }
 
