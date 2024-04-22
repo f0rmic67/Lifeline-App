cce7cf45c69a01d5bc88e1f6469952729a650ea7
cbeatty42 <cbeattyxy@gmail.com>
2024-04-21 21:48:51 -0400

    Updated history fragment to send user to the info page of the student whose id corresponds to the search item in the history when clicked.

--- app/src/main/java/com/lifeline/fragments/History.kt
+++ app/src/main/java/com/lifeline/fragments/History.kt
@@ -2,18 +2,21 @@ package com.lifeline.fragments
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.ImageButton
 import android.widget.ListView
 import android.widget.Toast
-import com.lifeline.R
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.lifeline.ApiUtils
+import com.lifeline.R
 import com.lifeline.SearchDataAdapter
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+
 
 class History : Fragment() {
     override fun onCreateView(
@@ -27,17 +30,25 @@ class History : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val backButton = requireView().findViewById<ImageButton>(R.id.historyBackButton)
+        backButton.setOnClickListener {
+            parentFragmentManager.popBackStack()
+        }
         // Start a coroutine in onViewCreated
         viewLifecycleOwner.lifecycleScope.launch(Dispatchers.Main) {
-            val serverResponse = ApiUtils.getRecentSearches(requireContext())
-            if(serverResponse == null){
+            val searchDataList = ApiUtils.getRecentSearches(requireContext())
+            if(searchDataList == null){
                 Toast.makeText(context, "No response from server", Toast.LENGTH_SHORT).show()
             }
             else{
-                Log.d("History", "$serverResponse")
+                Log.d("History", "$searchDataList")
                 val listView: ListView = view.findViewById(R.id.history_list)
-                val adapter = SearchDataAdapter(requireContext(), serverResponse)
+                val adapter = SearchDataAdapter(requireContext(), searchDataList)
                 listView.adapter = adapter
+
+                listView.onItemClickListener = OnItemClickListener { _, _, position, _ ->
+                    navigateToFragment(ViewStudentInfo.newInstance(searchDataList[position].student_id.toLong()))
+                }
 
             }
         }
