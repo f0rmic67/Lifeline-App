4e2394b320aa283e39b1c831683417d101199d27
cbeatty42 <cbeattyxy@gmail.com>
2024-04-23 13:49:57 -0400

    Eliminated code that allowed for cleartext.

--- app/src/main/java/com/lifeline/APIClient.kt
+++ app/src/main/java/com/lifeline/APIClient.kt
@@ -10,7 +10,6 @@ object APIClient {
         get() {
             retrofit = Retrofit.Builder()
                 .baseUrl("https://lifeline-project.net:5000/")
-                // .baseUrl("http://10.0.2.2:5000/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
             return retrofit
